c465dcd093608bd0109a621aa81d86926c371f63
Widyanto <hendry.widyanto@abbott.com>
2024-04-11 15:26:52 -0700

    Add SQL

---
+++ apps/sql/database.py
@@ -0,0 +1,24 @@
+from os import getenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+SERVER = getenv("SQL_SERVER")
+USER = getenv("SQL_USERNAME")
+PASSWORD = getenv("SQL_PASSWORD")
+DATABASE_NAME = getenv("SQL_DATABASE_NAME")
+ODBC_VERSION = getenv("SQL_ODBC_VERSION")
+
+params_url = (
+    f"?driver=ODBC+Driver+{ODBC_VERSION}+for+SQL+Server"
+    "&Encrypt=yes&TrustServerCertificate=no"
+    "&Connection+Timeout=30"
+)
+connection_url = (
+    f"mssql+pyodbc://{USER}:{PASSWORD}@{SERVER}:1433/{DATABASE_NAME}{params_url}"
+)
+
+engine = create_engine(connection_url, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+

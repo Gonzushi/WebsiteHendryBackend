bb15a7c4cc7057bcd652ed2743381f6ad32356c6
Hendry Widyanto <hendrywidyanto97@gmail.com>
2024-04-17 14:48:29 -0700

    asd

--- server.py
+++ server.py
@@ -4,7 +4,10 @@ import platform
 import uvicorn
 from fastapi import FastAPI
 
+from apps.project_crud import api as project_crud_api
+
 app = FastAPI()
+app.include_router(project_crud_api.router)
 
 
 @app.get("/")
@@ -15,7 +18,21 @@ if ! [[ "18.04 20.04 22.04 23.04" == *"$(lsb_release -rs)"* ]];
 then
     echo "Ubuntu $(lsb_release -rs) is not currently supported.";
     exit;
-fi"""
+fi
+
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+# optional: for bcp and sqlcmd
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+sudo apt-get install -y unixodbc-dev
+"""
         os.system(command)
     except Exception as e:
         return {"message": str(e)}

d1c6ecbc52e9219b0752e4b31f5ae7154254dda5
Hendry Widyanto <hendrywidyanto97@gmail.com>
2024-04-17 14:27:40 -0700

    Cyclic

--- check.py
+++ check.py
@@ -1,4 +1,5 @@
 import platform
+import uvicorn
 
 from fastapi import FastAPI
 
@@ -10,3 +11,7 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": str(platform.uname())}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8181)
